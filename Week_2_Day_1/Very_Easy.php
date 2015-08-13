ec1498e2a9feed2227f392e25087334d4333be38
Mummie <sethhandy39@gmail.com>
2015-08-12 21:03:58 -0400

    Create Very_Easy.php

---
+++ Week_2_Day_1/Very_Easy.php
@@ -0,0 +1,25 @@
+<!-- 
+
+    Using everything you have learned and some googling
+
+    Sum the numbers between 1 and 20 and display the result
+
+ -->
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <p>
+
+        <?php
+
+            for($i = 1; $i < 20; $i++){
+                $sum += $i;
+            }
+            echo $sum;
+
+
+        ?>
+    </p>
+  </body>
+</html>
