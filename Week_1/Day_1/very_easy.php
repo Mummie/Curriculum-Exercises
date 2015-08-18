1b6434ba1881263b873de558ac316893dd1f4e98
Mummie <sethhandy39@gmail.com>
2015-08-18 13:43:16 -0400

    Create very_easy.php

---
+++ Week_1/Day_1/very_easy.php
@@ -0,0 +1,33 @@
+<!-- 
+  Using everything you have learned and some googling
+
+  I have an array of names below but they are all out of order 
+  figure out the code needed to put them in order
+
+  Bonus: render all of the names in reverse order
+ -->
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+        <p>
+          <?php
+            $unOrderedArray = [
+              'Joseph',
+              'Lauren',
+              'Eric',
+              'James',
+              'Derek',
+              'Mark',
+              'Alex',
+              'Tru',
+              'Michael'
+            ];
+          sort($unOrderedArray);
+          print_r($unOrderedArray);
+          rsort($unOrderedArray);
+          print_r($unOrderedArray);
+          ?>
+        </p>
+  </body>
+</html>
