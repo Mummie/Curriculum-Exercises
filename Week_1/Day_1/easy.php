2f880b4a96c3e92a803dcdba739fd6fc3004ced5
Mummie <sethhandy39@gmail.com>
2015-08-18 13:39:55 -0400

    easy.php

---
+++ Week_1/Day_1/easy.php
@@ -0,0 +1,26 @@
+<!-- 
+	Using everything you have learned and some googling
+
+  Imagine this is a post page and they have sent you a number
+  you want to return them the number it text format. For instance:
+    
+    - 1 -> one
+    - 2 -> two
+
+  but anything that is above 10 we want to return "Nothing is greater than 10"
+ -->
+
+<!DOCTYPE html>
+<html>
+  <head></head>
+	<body>
+        <p>
+          <?php
+            $number = 3 // this came from the previous page as a post variable
+
+          	strval($number);
+          	echo $number;
+          ?>
+        </p>
+	</body>
+</html>
